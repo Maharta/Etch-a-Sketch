568a5ba2c12816421bad71501d618b50f53f60b7
Maharta <gedemaharta.dev@gmail.com>
2022-09-18 22:18:21 +0800

    added functionalities for the settings

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const sketchContainer = document.querySelector(".sketch__container");
+const rangeInput = document.getElementById("range");
+const colorInput = document.getElementById("color");
+const rangeLabel = document.querySelector("label[for='range']");
 
-function makeGrid(numberOfRows = 16, numberOfCols = 16) {
-  sketchContainer.style.setProperty("--grid-rows", numberOfRows);
-  sketchContainer.style.setProperty("--grid-cols", numberOfCols);
-  for (let i = 0; i < numberOfCols * numberOfRows; i++) {
+let penColor = "#000000";
+
+function makeGrid(gridNumber = 16) {
+  sketchContainer.innerHTML = "";
+  rangeLabel.textContent = `${gridNumber} x ${gridNumber}`;
+  sketchContainer.style.setProperty("--grid-rows", gridNumber);
+  sketchContainer.style.setProperty("--grid-cols", gridNumber);
+  for (let i = 0; i < gridNumber * gridNumber; i++) {
     const div = document.createElement("div");
     div.classList.add("grid");
+    div.addEventListener("mouseover", paint);
+    div.addEventListener("click", paint);
     sketchContainer.appendChild(div);
   }
 }
 
-makeGrid();
+function paint(e) {
+  console.log(e);
+  if (e.buttons === 1) {
+    this.style.backgroundColor = penColor;
+  }
+}
+
+rangeInput.addEventListener("input", (e) => {
+  makeGrid(e.target.value);
+});
+
+colorInput.addEventListener("change", (e) => {
+  penColor = e.target.value;
+  console.log(penColor);
+});
+
+window.onload = makeGrid();
